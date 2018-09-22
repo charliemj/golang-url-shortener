8fd8ea94c87a821cd2cfd89658b9a5cea9bd417e
Karleigh Moore <karleighmoore19@gmail.com>
2018-09-22 18:00:46 -0400

    changed away from gorm because cannot test gorm

--- main.go
+++ main.go
@@ -1,43 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/speps/go-hashids"
-	//"net/http"
 	"time"
 )
 
-var db *gorm.DB
-var err error
-
-type MyUrl struct {
-	ID       string `json:"id,omitempty"`
-	LongUrl  string `json:"longUrl,omitempty"`
-	ShortUrl string `json:"shortUrl,omitempty"`
-}
+var db *sql.DB
 
 func main() {
 
 	//Set up database
-	db, err = gorm.Open("sqlite3", "./gorm.db")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
+	MakeDatabase()
 
-	db.AutoMigrate(&MyUrl{})
-
-	// p1 := MyUrl{LongUrl: "http://charlie.codes", ID: "1"}
-	// p2 := MyUrl{LongUrl: "http://kar.moore.com", ID: "2"}
-	// db.Create(&p1)
-	// db.Create(&p2)
-
-	//Routing
+	//Set up routers
 	router := MakeRouter()
 	router.Run(":8080")
+}
+
+func MakeDatabase() {
+	db, _ = sql.Open("sqlite3", "./urls.db")
+	defer db.Close()
+	create := `create table if not exists urls (longUrl text, short text, id text primary key);`
+	db.Exec(create)
 }
 
 //returns a router
@@ -47,39 +34,53 @@ func MakeRouter() *gin.Engine {
 	r.LoadHTMLFiles("index.html", "show.html")
 	r.GET("/:id", ExpandUrl)
 	r.POST("/create", CreateShortUrl)
-
 	return r
 }
 
-//Handles GET request for the homepage. Returns index.html
+//Handles GET request for the homepage. Returns index.html.
 func GetIndex(c *gin.Context) {
 	c.HTML(200, "index.html", gin.H{})
+}
+
+func getOriginalUrl(id string) string {
+	db, _ := sql.Open("sqlite3", "./urls.db")
+	defer db.Close()
+
+	var longUrl string
+
+	query, _ := db.Prepare("select longUrl from urls where id = ?")
+	query.QueryRow(id).Scan(&longUrl)
+
+	return longUrl
 }
 
 //Handles GET requests for shortened urls. Resolves the shortened url
 //to its long url counterpart and redirects to that url.
 func ExpandUrl(c *gin.Context) {
 	id := c.Params.ByName("id")
-	var url MyUrl
-	if err := db.Where("id = ?", id).First(&url).Error; err != nil {
+	var longUrl string
+	longUrl = getOriginalUrl(id)
+
+	if longUrl == "" {
 		c.AbortWithStatus(404)
-		fmt.Println(err)
 	} else {
-		c.Redirect(301, url.LongUrl)
+		c.Redirect(301, longUrl)
 	}
 }
 
 //Handles POST request. for creating a new short url.
 func CreateShortUrl(c *gin.Context) {
+	db, _ := sql.Open("sqlite3", "./urls.db")
+	defer db.Close()
+
 	var longUrl string
 	longUrl = c.PostForm("longUrl")
-	var id string
-	var short string
+	var id, short string
 	id = MakeUniqueSlug()
 	short = "http:localhost:8080/" + id
-	var newUrl MyUrl
-	newUrl = MyUrl{LongUrl: longUrl, ShortUrl: short, ID: id}
-	db.Create(&newUrl)
+
+	db.Exec("INSERT into urls(longUrl, short, id) values(?,?,?)", longUrl, short, id)
+
 	c.HTML(200, "show.html", gin.H{"short": short})
 }
 
@@ -87,31 +88,9 @@ func CreateShortUrl(c *gin.Context) {
 //short urls
 func MakeUniqueSlug() string {
 	var id string
-	var url MyUrl
-	var isAlreadyMade bool
-	isAlreadyMade = true
-
 	hd := hashids.NewData()
 	h, _ := hashids.NewWithData(hd)
-	//check if id is already in db and keep trying to make on until it is not
-	for isAlreadyMade {
-		now := time.Now()
-		id, _ = h.Encode([]int{int(now.Unix())})
-
-		//get all rows where the id is = the id we just made
-		rows, _ := db.Where(map[string]interface{}{"id": id}).Find(&url).Rows()
-
-		//if there are any rows with that id, we remake the id
-		count := 0
-		for rows.Next() {
-			count++
-		}
-		if count == 0 {
-
-			isAlreadyMade = false
-		} else {
-			isAlreadyMade = true
-		}
-	}
+	now := time.Now()
+	id, _ = h.Encode([]int{int(now.Unix())})
 	return id
 }
